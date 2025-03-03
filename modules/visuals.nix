9122e1620a074e2b1f51c71e56ded4ec9175b4bf
TheColorman <github@colorman.me>
2025-03-03 22:42:48 +0100

    visuals/fidget: disable messages from null-ls
    gitsigns causes null-ls to print constant diagnostics, which is
    annoying. Should be able to ignore only messages that contain
    code_action when TODO is implemented

--- modules/visuals.nix
+++ modules/visuals.nix
@@ -2,7 +2,14 @@
 {
   vim.visuals = {
     cinnamon-nvim.enable = true;
-    fidget-nvim.enable = true;
+    fidget-nvim = {
+      enable = true;
+      # @TODO 2025-04-03: this should support functions, see https://github.com/j-hui/fidget.nvim/issues/198#issuecomment-2212264394.
+      # Implement in upstream.
+      setupOpts.progress.ignore = [
+        "null-ls"
+      ];
+    };
     highlight-undo.enable = true;
     indent-blankline.enable = true;
     nvim-cursorline.enable = true;
