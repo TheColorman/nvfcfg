5fb23f34f26c657ba8584dfdd36c227896483163
TheColorman <github@colorman.me>
2025-03-28 21:06:25 +0100

    Add colorman.cachix.org to substitutors

--- flake.nix
+++ flake.nix
@@ -1,42 +1,47 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    alejandra = {
-      url = "github:kamadorueda/alejandra";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+		nvf = {
+			url = "github:notashelf/nvf";
+			inputs.nixpkgs.follows = "nixpkgs";
+			inputs.flake-utils.follows = "flake-utils";
+		};
+		alejandra = {
+			url = "github:kamadorueda/alejandra";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		flake-utils.url = "github:numtide/flake-utils";
+	};
 
-  outputs = {
-    self,
-    nixpkgs,
-    nvf,
-    flake-utils,
-    ...
-  } @ inputs: (flake-utils.lib.eachDefaultSystem (
-    system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+	outputs = {
+		self,
+		nixpkgs,
+		nvf,
+		flake-utils,
+		...
+	} @ inputs: (flake-utils.lib.eachDefaultSystem (
+			system: let
+				pkgs = nixpkgs.legacyPackages.${system};
 
-      mkNvim = config:
-        (nvf.lib.neovimConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {parentInputs = inputs;};
-          modules = [config];
-        })
-        .neovim;
-    in {
-      packages = {
-        nvim = mkNvim ./configurations/main.nix;
-        default = self.outputs.packages.${system}.nvim;
+				mkNvim = config:
+					(nvf.lib.neovimConfiguration {
+							inherit pkgs;
+							extraSpecialArgs = {parentInputs = inputs;};
+							modules = [config];
+						})
+					.neovim;
+			in {
+				packages = {
+					nvim = mkNvim ./configurations/main.nix;
+					default = self.outputs.packages.${system}.nvim;
 
-        qwerty = mkNvim ./configurations/qwerty.nix;
-      };
-    }
-  ));
+					qwerty = mkNvim ./configurations/qwerty.nix;
+				};
+			}
+		));
+
+	nixConfig = {
+		extra-substituters = ["https://colorman.cachix.org"];
+		extra-trusted-public-keys = ["colorman.cachix.org-1:HbmIr3Upj+ghnP8EYogEn73eCUSDeFBJ+JoItkw8EbQ="];
+	};
 }
