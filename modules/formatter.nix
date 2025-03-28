2028d428f70fc5c493745018483a0f902496e1d4
TheColorman <github@colorman.me>
2025-03-28 18:14:50 +0100

    update nix formatter

--- modules/formatter.nix
+++ modules/formatter.nix
@@ -1,16 +1,27 @@
 {
-  vim = {
-    formatter.conform-nvim = {
-      enable = true;
-      setupOpts.formatters_by_ft = {
-        lua = ["stylua"];
-        cs = ["csharpier"];
-        css = ["prettier"];
-        html = ["prettier"];
-        vue = ["prettier"];
-        typescript = ["prettier"];
-      };
-    };
-    languages.enableFormat = true;
-  };
+	lib,
+	pkgs,
+	parentInputs,
+	...
+}: {
+	vim = {
+		formatter.conform-nvim = {
+			enable = true;
+			setupOpts = {
+				formatters_by_ft.nix = ["alejandra"];
+				formatters = {
+					alejandra = {
+						command = lib.getExe parentInputs.alejandra.defaultPackage.${pkgs.stdenv.hostPlatform.system};
+						args = ["--experimental-config" ../alejandra.toml];
+					};
+				};
+				format_on_save = {
+					lsp_format = "fallback";
+					timeout_ms = 500;
+				};
+				format_after_save.lsp_format = "fallback";
+			};
+		};
+		languages.enableFormat = true;
+	};
 }
