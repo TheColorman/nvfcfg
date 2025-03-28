5ffa43eaa607e0be0bf6760fb83ac3be9e7bba54
TheColorman <github@colorman.me>
2025-03-28 13:29:07 +0100

    Add clipboard

--- modules/utility.nix
+++ modules/utility.nix
@@ -1,19 +1,22 @@
-{
-  vim.utility = {
-    vim-wakatime.enable = true;
-    diffview-nvim.enable = true;
-    yanky-nvim.enable = false;
-    icon-picker.enable = true;
-    surround.enable = true;
-    leetcode-nvim.enable = true;
-    multicursors.enable = true;
+{pkgs, ...}: {
+  vim = {
+    utility = {
+      vim-wakatime.enable = true;
+      diffview-nvim.enable = true;
+      yanky-nvim.enable = false;
+      icon-picker.enable = true;
+      surround.enable = true;
+      leetcode-nvim.enable = true;
+      multicursors.enable = true;
 
-    motion = {
-      hop.enable = true;
-      leap.enable = true;
-      precognition.enable = true;
+      motion = {
+        hop.enable = true;
+        leap.enable = true;
+        precognition.enable = true;
+      };
+
+      # images.image-nvim.enable = true; # FIXME: doesn't work in wsl
     };
-
-    # images.image-nvim.enable = true; # FIXME: doesn't work in wsl
+    extraPackages = with pkgs; [wl-clipboard];
   };
 }
