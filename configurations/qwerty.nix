736b9eb1a23b7237a9512c197c5061078a1b8e10
Alexander Thoren <act@uxvtechnologies.com>
2025-03-31 16:32:07 +0200

    change formatter and add vue support

--- configurations/qwerty.nix
+++ configurations/qwerty.nix
@@ -1,18 +1,17 @@
 {
-	imports = [
-		../modules/mappings/quick.nix
+  imports = [
+    ../modules/mappings/quick.nix
 
-		../modules/common.nix
-		../modules/languages.nix
-		../modules/lsp.nix
-		../modules/debugger.nix
-		../modules/formatter.nix
-		../modules/visuals.nix
-		../modules/typing.nix
-		../modules/utility.nix
-		../modules/assistant.nix
-	];
+    ../modules/common.nix
+    ../modules/languages.nix
+    ../modules/lsp.nix
+    ../modules/debugger.nix
+    ../modules/visuals.nix
+    ../modules/typing.nix
+    ../modules/utility.nix
+    ../modules/assistant.nix
+  ];
 
-	# Discord rich presence
-	vim.presence.neocord.enable = false;
+  # Discord rich presence
+  vim.presence.neocord.enable = false;
 }
--- modules/languages.nix
+++ modules/languages.nix
@@ -1,53 +1,57 @@
 {
-	# List of languages to enable formatting and LSP support for
-	vim.languages = {
-		# What to enable
-		enableDAP = true;
-		enableExtraDiagnostics = true;
-		enableFormat = true;
-		enableLSP = true;
-		enableTreesitter = true;
+  imports = [./languages/ts.nix];
 
-		# Languages to enable it for
-		assembly.enable = true;
-		bash.enable = true;
-		clang.enable = true;
-		csharp.enable = true;
-		css.enable = true;
-		html.enable = true;
-		lua.enable = true;
-		markdown = {
-			enable = true;
-			extensions.render-markdown-nvim.enable = true;
-		};
-		nix = {
-			enable = true;
-			format.enable = false;
-			lsp = {
-				server = "nil";
-				# @TODO: For some reasons nvf does not support passing options to nil?
-				# options = {
-				#   formatting.command = "${lib.getExe pkgs.alejandra} --experimental-config ${../alejandra.toml}";
-				#   nix = {
-				#     maxMemoryMB = 8192; # 8 GiB
-				#     flake = {
-				#       autoArchive = true;
-				#       autoEvalInputs = true;
-				#     };
-				#   };
-				# };
-			};
-		};
-		python = {
-			enable = true;
-			format.type = "ruff";
-		};
-		svelte.enable = true;
-		tailwind.enable = true;
-		ts = {
-			enable = true;
-			extensions.ts-error-translator.enable = true;
-		};
-		yaml.enable = true;
-	};
+  # List of languages to enable formatting and LSP support for
+  vim.languages = {
+    # What to enable
+    enableDAP = true;
+    enableExtraDiagnostics = true;
+    enableFormat = true;
+    enableLSP = true;
+    enableTreesitter = true;
+
+    # Languages to enable it for
+    assembly.enable = true;
+    bash.enable = true;
+    clang.enable = true;
+    csharp.enable = true;
+    css.enable = true;
+    html.enable = true;
+    lua.enable = true;
+    markdown = {
+      enable = true;
+      extensions.render-markdown-nvim.enable = true;
+    };
+    nix = {
+      enable = true;
+      lsp = {
+        server = "nil";
+        # @TODO: For some reasons nvf does not support passing options to nil, only nixd?
+        # options = {
+        #   nix = {
+        #     maxMemoryMB = 8192; # 8 GiB
+        #     flake = {
+        #       autoArchive = true;
+        #       autoEvalInputs = true;
+        #     };
+        #   };
+        # };
+      };
+    };
+    python = {
+      enable = true;
+      format.type = "ruff";
+    };
+    svelte = {
+      enable = true;
+      extraDiagnostics.enable = false;
+    };
+    tailwind.enable = true;
+    # NOTE: TS support manually set in ./languages/ts.nix due to missing Vue support in nvf
+    # ts = {
+    # 	enable = true;
+    # 	extensions.ts-error-translator.enable = true;
+    # };
+    yaml.enable = true;
+  };
 }
